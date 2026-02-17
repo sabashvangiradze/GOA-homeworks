9aad7d57e5538039e81d578ec04c60e6d92ec8fb
sabashvangiradze <sabashvangiradze24@gmail.com>
2026-02-17 17:13:53 +0400

    commit

---
+++ day02/homework/homework.py
@@ -0,0 +1,72 @@
+# string - ეს არის ერთ-ერთი მონაცემთა ტიპი რომელიც არის მონაცემთ ტიპი ,ბრჭყალებში მოცემული რიცხვების ასოების და სიმბოლოების ნაკრები   
+#integer - ეს არის ერთ-ერთი მონაცემთა ტიპი რომელიც არის მთელი რიცხვი
+#float - ეს არის ერთ-ერთი მონაცემთა ტიპი რომელიც არის წილადი,ათწილადი და არამთელი რიცხვი
+
+
+#ცვლადი ანუ variable არის დასახელებული "კონტეინერი", ის გამოიყენება იმისთვის რომ მასში მონაცემები შევინახოთ
+
+
+#string
+
+print("saba")
+print("manqana")
+print("vashli")
+print("goa-goal oriented academy")
+print("programming")
+
+
+#integer
+
+print(9087)
+print(325769)
+print(214698)
+print(4719064)
+print(12315)
+
+#float
+
+print(10.1)
+print(98.3)
+print(227.4)
+print(78.5)
+print(21.2)
+
+
+#Making one large sentence using 5 variables with only one print()
+
+name = "Saba"
+age = "15"
+height = "180"
+color = "blue"
+job = "programming"
+print("Hello, my name is " + name + " im " + age + " years old and my height is " + height + " my favourite color is " + color + " and my future job is " + job )
+
+
+#Mathematical operations
+
+print(93856+37648)
+print(230456+37596)
+print(14763+47649)
+
+
+print(8907562-7455)
+print(248673926-7659693)
+print(32408364-78536)
+
+
+print(87*37)
+print(64*99)
+print(32*56)
+
+
+print(81/9)
+print(400/20)
+print(1024/32)
+
+
+#Doing addition with varirables 
+
+num1 = 33
+num2 = 74
+num3 = 13
+print(num1 + num2 + num3)
