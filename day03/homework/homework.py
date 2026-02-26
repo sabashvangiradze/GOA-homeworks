a5d17c3196cf1e73ebd55196ae9f8f20897e266a
sabashvangiradze <sabashvangiradze24@gmail.com>
2026-02-26 21:05:35 +0400

    commit

---
+++ day03/homework/homework.py
@@ -0,0 +1,38 @@
+#კომენტარი არის კოდი რომელიც მხოლოდ ადამიანს შეუძლია წაიკითხოს და ამას terminal ყურადღებას არ მიაქცევს კომენტარი იწერება დიეზით ( # )
+#case-sensitive ისეთი პროგრამირების ენებია სადაც ყურადღებას აქცევს კომპიუტერი იმას თუ როგორ რეგისტრშია კოდი და ამ ენაში კოდი უნდა დაიწეროს პატარა ასოთი
+#case-insensitive ისეთი პროგრამირების ენებია სადაც ყურადღებას არ აქცევს კომპიუტერი იმას თუ როგორ რეგისტრშია კოდი ამიტომ ამ ენაში კოდი დიდი და პატარა ასოთიც იწერება 
+#Snake_case არის ტექსტის ჩაწერის სტილი პროგრამირებაში, სადაც კოდი პატარა ასოთი იწერება და წინადადება იყოფა ქვედატირეთი ( _ )
+
+user_name = "Saba"
+user_name2 = " Shvangiradze"
+user_age = "15"
+user_favourite_color = "blue"
+user_height = "180cm"
+print("Hello,my name is " + user_name + user_name2 + ",im teenager aged " + user_age + ",my favourite color is " + user_favourite_color + ",and my height is " + user_height + ",thanks for listening! ")
+
+#bug ეს არის კოდში არსებული შეცდომა,ამავდროულად bug არის ჭია,რომელიც შეძვრა კომპიუტერში და კაბელი დააზიანა მისი ისტორიის თანახმად
+#debugging-ეს არის კოდში არსებული შეცდომის გამოსწორება
+
+#გამოსწორებული შეცდომები კოდებში
+
+name = "Lika"
+print(name)
+
+number = "5"
+text = " apples"
+result = number + text
+print(result)
+
+name = "Lika"
+name2 = name + "4"
+
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
+
+first_name = "Data"
+last_name = "random"
+
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
