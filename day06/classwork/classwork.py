e1fb1302a649b06bbaefd59f9dcb1494e19951f4
sabashvangiradze <sabashvangiradze24@gmail.com>
2026-03-15 15:57:26 +0400

    commit

---
+++ day06/classwork/classwork.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+print(name)
+
+
+num1 = int(input("Enter any number: "))
+num2 = int(input("Enter any nunber: "))
+print(num1 + num2)
+
+age = int(input("Enter your age: "))
+print(age)
+print(type(age))
+
+
+name = "Saba"
+age = 15
+weight = 63.2
+print(type(name))
+print(type(age))
+print(type(weight))
---
+++ day06/exam01/exam01.py
@@ -0,0 +1,28 @@
+# num1 = input("Enter your age: ")
+# name = input("Enter your name: ")
+# print(num1 + name)
+
+
+# user_firstname = "saba "
+# user_lastname = "shvangiradze"
+# user = user_firstname + user_lastname
+# print(user)
+
+# str
+# int
+# float
+# bool
+# type
+
+
+number1 = input("Enter your number: ")
+number2 = input("Enter your number: ")
+number3 = int(number1) + int(number2)
+print(number3)
+
+number4 = input("Enter your number: ")
+number5 = input("Enter your number: ")
+number6 = int(number4) + int(number5)
+print(number6)
+
+print(number3 >= number6)
